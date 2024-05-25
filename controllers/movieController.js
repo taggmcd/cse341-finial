49e1673d5689170f5e7bfd2722bb171d1462694e
Tagg McDonald <taggmcd@gmail.com>
2024-05-25 12:33:48 -0600

    Adjust Model name in delete function

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,11 @@
 const { response } = require('express');
 const mongoose = require('mongoose');
 const Movies = require('../models/movie');
-const { getAndSendAll, getAndSendOne, deleteItemAndSendMessage } = require("./baseController");
+const {
+  getAndSendAll,
+  getAndSendOne,
+  deleteItemAndSendMessage,
+} = require('./baseController');
 
 const index = async (req, res) => {
   //#swagger.tags = ['Movie']
@@ -19,30 +23,36 @@ const store = async (req, res) => {
   //#swagger.tags = ['Movie']
   // Create a new movie in mongodb
   try {
-    const { title, releaseYear, genres, runtime, rated, plot, posterUrl } = req.body;
+    const { title, releaseYear, genres, runtime, rated, plot, posterUrl } =
+      req.body;
     const newMovie = new Movies({
-        title,
-        releaseYear,
-        genres,
-        runtime,
-        rated,
-        plot,
-        posterUrl,
+      title,
+      releaseYear,
+      genres,
+      runtime,
+      rated,
+      plot,
+      posterUrl,
     });
     const savedMovie = await newMovie.save();
     res.status(201).json(savedMovie);
-} catch (error) {
+  } catch (error) {
     res.status(500).json({ error: 'Error creating movie' });
-}
+  }
 };
 
 const update = async (req, res) => {
   //#swagger.tags = ['Movie']
   // Update a movie in mongodb
   const movieId = req.params.id;
-  const { title, releaseYear, genres, runtime, rated, plot, posterUrl } = req.body;
+  const { title, releaseYear, genres, runtime, rated, plot, posterUrl } =
+    req.body;
   try {
-    const movie = await Movies.findByIdAndUpdate(movieId, {title, releaseYear, genres, runtime, rated, plot, posterUrl}, { new: true });
+    const movie = await Movies.findByIdAndUpdate(
+      movieId,
+      { title, releaseYear, genres, runtime, rated, plot, posterUrl },
+      { new: true }
+    );
     res.status(204).json(movie);
   } catch (error) {
     res.status(400).send(error);
@@ -52,7 +62,7 @@ const update = async (req, res) => {
 const destroy = async (req, res) => {
   //#swagger.tags = ['Movie']
   // Delete a movie from mongodb
-  deleteItemAndSendMessage(req, res, Movie);
+  deleteItemAndSendMessage(req, res, Movies);
 };
 
 module.exports = { index, show, store, update, destroy };
