dac961d33761c2a7c44ac26d71a70c92e31c54c7
adriankippins <robertokippins@iotgy.net>
2024-06-01 23:04:51 -0400

    Addes Login and Logout Configuration to swagger.js

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     version: '1.0.0',
     title: 'Media Tracking API',
     description:
-      'A RESTful API to keep track of the movies, series, and books you are watching and reading.',
+      "A RESTful API to keep track of the movies, series, and books you are watching and reading.\n<a href='https://cse341-finial.onrender.com/login' target='_blank'>Login With GitHub<a>\n<a href='https://cse341-finial.onrender.com/logout' target='_blank'>Log Out<a>",
   },
   host: 'cse341-finial.onrender.com',
   schemes: ['https', 'http'],
